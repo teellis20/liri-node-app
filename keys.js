ef1511ec26fa2c66c340e7aaee78a41f40ef6737
Tellis <teellis20@gmail.com>
2019-10-02 23:50:27 -0700

    almost done

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+
+// id   af45adb4a87e4cb6abdc13abe7709a0d
+// secret 22a7339668c84020b07453bd65b70c0f
---
+++ liri.js
@@ -0,0 +1,116 @@
+require("dotenv").config();
+var axios = require("axios");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+
+var spotify = new Spotify(keys.spotify);
+
+
+var divider = "***************"
+var command = process.argv[2];
+var criteria = process.argv.slice(3).join(" ");
+var bandURL = "https://rest.bandsintown.com/artists/" + criteria + "/events?app_id=codingbootcamp";
+// var movieURL = "http://www.omdbapi.com/?t=" + criteria + "&y=&plot=short&apikey=trilogy";
+
+
+function findBand() {
+    axios.get(bandURL).then(function(response) {
+        var band = response.data;
+        var bandData = [
+            "Venue: " + band[0].venue.name,
+            "Venue Location:" + band[0].venue.city + ", " + band[0].venue.country,
+            "Date: " + band[0].datetime
+        ].join("\n\n");
+
+        fs.appendFile("log.txt", bandData + divider, function(err) {
+            if (err) throw err;
+            console.log(bandData);
+        })
+    })
+};
+
+function findSong() {
+    spotify.search({ type: "track", query: criteria}, function(err, data) {
+    if (err) {
+        return console.log("Error occurred: " + err);
+    }
+        
+        var songData = [ 
+            "Artist(s): " + data.tracks.items[0].artists[0].name,
+            "Song Title: " + data.tracks.items[0].name,
+            "Preview Link: " + data.tracks.items[0].preview_url,
+            "Album: " +  data.tracks.items[0].album.name,
+        ].join("\n\n");
+
+        // console.log(songData);
+        fs.appendFile("log.txt", songData + divider, function(err) {
+            if (err) throw err;
+            console.log(songData);
+    });
+});
+
+};
+
+function findMovie() {
+    var movieURL = "http://www.omdbapi.com/?t=" + criteria + "&y=&plot=short&apikey=trilogy";
+
+    if (!criteria) {
+        criteria = "bohemian rhapsody";
+        findMovie();
+    } else {
+    axios.get(movieURL).then(function(response) {
+    var movie = response.data;
+    var movieData = [
+        "Title: " + movie.Title,
+        "Year: " + movie.Year,
+        "IMDB Rating: " + movie.imdbRating,
+        "Country: " + movie.Country,
+        "Language: " + movie.Language,
+        "Plot: " + movie.Plot,
+        "Actors: " + movie.Actors
+    ].join("\n\n");
+
+    fs.appendFile("log.txt", movieData + divider, function(err) {
+        if (err) throw err;
+        console.log(movieData);
+    })
+
+    });
+    }
+};
+
+function doIt() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        data = data.split(", ");
+        
+        // criteria = data;
+        console.log(data);
+        command = data[0];
+        criteria = data[1];
+        console.log(command);
+        console.log(criteria)
+    });
+};
+
+
+if (command === "concert-this") {
+    findBand();
+} 
+else if (command === "spotify-this-song") {
+    if (!criteria) {
+        criteria = "Crazy";
+        findSong();
+    } else {
+    findSong();
+    }
+}
+ else if (command === "movie-this") {
+    findMovie();
+} 
+else if (command === "do-what-it-says") {
+    doIt();
+}
